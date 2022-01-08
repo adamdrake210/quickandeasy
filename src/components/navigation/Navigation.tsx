11aa7dccfd1603772d064ea0647e5f40f1b62556
Adam Drake <adamgedrake@gmail.com>
2022-01-08 07:17:13 +0100

    feature: mobile navigation (#7)
    Co-authored-by: Adam Drake <addrake@ebay.com>

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 import {
@@ -35,15 +35,16 @@ const MenuItems = [
 ];
 
 export const Navigation = () => {
+  const [showMenu, setShowMenu] = useState(false);
   const router = useRouter();
 
   return (
-    <nav className="bg-slate-50 left-0 right-0 font-raleway shadow-md fixed z-10">
+    <nav className="bg-slate-50/95 left-0 right-0 font-raleway shadow-md fixed z-10">
       <div className="max-w-screen-lg mx-auto">
         <div className="flex justify-between p-3">
           <div className="flex grow items-center">
             <Link href={HOME} passHref>
-              <span className="font-staatliches text-4xl text-green-700 font-extrabold uppercase cursor-pointer mr-8">
+              <span className="font-staatliches text-5xl text-green-700 font-extrabold uppercase cursor-pointer mr-8">
                 {COMPANY_NAME}
               </span>
             </Link>
@@ -66,7 +67,67 @@ export const Navigation = () => {
               );
             })}
           </div>
+
+          {/* Mobile Hamburger icon */}
+          <div className="md:hidden flex items-center">
+            <button
+              className="outline-none"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              {showMenu ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
+        {/* Mobile Menu */}
+        {showMenu && (
+          <div className="flex flex-col pb-2">
+            {MenuItems.map((item, index) => {
+              return (
+                <Link href={item.url} passHref key={index}>
+                  <div
+                    className={`text-3xl font-staatliches uppercase hover:text-green-600 hover:underline py-3 px-4 ${
+                      router.asPath === item.url
+                        ? "text-green-600 underline"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    <p>{item.label}</p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        )}
       </div>
     </nav>
   );
---
+++ todo.md
@@ -0,0 +1,5 @@
+- kontakt
+- footer details - facebook, address etc
+- Homepage - all
+- menu - images, extras
+- put on vercel
